78c3f38c0e23fef65598120c51e8ebae3149e78d
canemrecn <emrecancnzytnl@gmail.com>
2024-02-24 13:45:49 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,16 @@
+// Belirli bir eşik değerini tanımlayın. Bu değeri ne zaman aşarsanız sayfa aşağı kayacak.
+
+
+// Kaydırma olayını dinleyin
+window.addEventListener('scroll', () => {
+    // Mevcut kaydırma konumunu alın
+    const scrollY = window.scrollY;
+
+    // Eğer mevcut kaydırma konumu eşik değerini aşıyorsa
+    if (scrollY > scrollThreshold) {
+        // Sayfayı aşağı doğru kaydırın
+        window.scrollBy(0, scrollThreshold);
+    }
+});
+
+
